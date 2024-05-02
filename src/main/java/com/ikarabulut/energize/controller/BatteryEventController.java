3fba4b537c3fa2e2970f5c404d4cc53a7c3ba065
Ismail Karabulut <i.karabulut93@gmail.com>
2024-05-02 16:50:59 -0700

    update POST to use Avro Model;

--- src/main/java/com/ikarabulut/energize/controller/BatteryEventController.java
+++ src/main/java/com/ikarabulut/energize/controller/BatteryEventController.java
@@ -1,13 +1,22 @@
 package com.ikarabulut.energize.controller;
 
 import com.codahale.metrics.annotation.Timed;
+import com.ikarabulut.energize.avro.BatteryEvent;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.time.Instant;
 import java.util.UUID;
+
+import static org.apache.commons.compress.utils.IOUtils.toByteArray;
 
 @Path("/event")
 @Produces(MediaType.APPLICATION_JSON)
@@ -18,9 +27,10 @@ public class BatteryEventController {
     @POST
     @Timed
     @Path("/{uuid}")
-    public String createEvent(@PathParam("uuid") UUID id, String event) {
-        System.out.println(event);
-        return id.toString();
+    public Response createEvent(@PathParam("uuid") UUID id, @Context HttpServletRequest request) throws IOException {
+        ByteBuffer body = ByteBuffer.wrap(toByteArray(request.getInputStream()));
+        BatteryEvent payload = new BatteryEvent(id.toString(), Instant.now().toEpochMilli(), body);
+        return Response.ok().entity(id).build();
     }
 
 
